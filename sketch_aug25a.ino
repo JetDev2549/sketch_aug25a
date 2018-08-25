b7788220ced3dd78de0e26274df7c447c564993f
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-08-25 13:10:12 +0700

    added firebase details

--- sketch_aug25a.ino
+++ sketch_aug25a.ino
@@ -1,6 +1,15 @@
 URL: arduino-23468.firebaseio.com
 Auth: PguRO8UGBa2Bw9o6aLyAor4H7qyQGpOpeQEa8Wrq
 
+#include <FirebaseArduino.h>
+#include <ESP8266WiFi.h>
+
+#define WIFI_SSID "heating spotttt"
+#define WIFI_PASSWORD "lmaoman123"
+
+#define FIREBASE_URL "arduino-23468.firebaseio.com"
+#define FIREBASE_AUTH "PguRO8UGBa2Bw9o6aLyAor4H7qyQGpOpeQEa8Wrq"
+
 void setup() {
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
