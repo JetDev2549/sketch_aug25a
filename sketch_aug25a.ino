3488cea7524294384162bc27b3f3825ac60be736
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-08-25 13:06:48 +0700

    llolololololo

--- sketch_aug25a.ino
+++ sketch_aug25a.ino
@@ -1,10 +1,22 @@
+URL: arduino-23468.firebaseio.com
+Auth: PguRO8UGBa2Bw9o6aLyAor4H7qyQGpOpeQEa8Wrq
+
 void setup() {
   pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
 }
 
 void loop() {
   digitalWrite(D1, LOW);
   delay(500);
   digitalWrite(D1, HIGH);
+  delay(500);
+  digitalWrite(D1, LOW);
+  delay(500);
+  digitalWrite(D2, LOW);
+  delay(500);
+  digitalWrite(D2, HIGH);
+  delay(500);
+  digitalWrite(D2, LOW);
   delay(500);
 }
