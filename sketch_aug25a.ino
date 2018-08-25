4f2e8e9adf5fcf4f476e71ff7cd64d9ce1a1bbb5
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-08-25 11:09:56 +0700

    Initial

--- sketch_aug25a.ino
+++ sketch_aug25a.ino
@@ -1,9 +1,10 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(D1, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(D1, LOW);
+  delay(500);
+  digitalWrite(D1, HIGH)
+  delay(500);
 }
