f05dc682aa90438acd3cfe9765c279ca58eee85d
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-09-01 15:17:40 +0700

    void loop

--- sep01c/sketch_sep01c/sketch_sep01c.ino
+++ sep01c/sketch_sep01c/sketch_sep01c.ino
@@ -8,5 +8,25 @@
 #define FIREBASE_URL "ionicarduino-2393a.firebaseio.com"
 #define FIREBASE_AUTH "lelyI1Z6qbaHrTaU39aYKXZCCUV9CPcT3jVLOVSA"
 
-const int ServoPin = D6;
+Servo myservo;
+
+void setup() {
+  
+ Serial.begin(115200);
+ Serial.println(WiFi.localIP());
+ WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+
+ Serial.println();
+ Serial.print("Connected.");
+ Serial.println(WiFi.localIP());
+
+ Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+ Firebase.setInt("ArDuInO/Servo",0);
+ myservo.attach(D6);
+
 
