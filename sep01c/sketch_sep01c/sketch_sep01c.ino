35fadea704b429c68c431072a234a1fc60c21820
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-09-01 15:08:02 +0700

    Added sep01c

---
+++ sep01c/sketch_sep01c/sketch_sep01c.ino
@@ -0,0 +1,12 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <Servo.h>
+
+#define WIFI_SSID "Jet"
+#define WIFI_PASSWORD "12345678"
+
+#define FIREBASE_URL "ionicarduino-2393a.firebaseio.com"
+#define FIREBASE_AUTH "lelyI1Z6qbaHrTaU39aYKXZCCUV9CPcT3jVLOVSA"
+
+const int ServoPin = D6;
+
