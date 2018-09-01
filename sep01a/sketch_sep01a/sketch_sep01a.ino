d67eed637152c9dc81a2202aef2611f141131551
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-09-01 11:55:44 +0700

    Changed Wi-Fi details

--- sep01a/sketch_sep01a/sketch_sep01a.ino
+++ sep01a/sketch_sep01a/sketch_sep01a.ino
@@ -5,8 +5,8 @@
 
 
 
-#define WIFI_SSID "Jet"
-#define WIFI_PASSWORD "12345678"
+#define WIFI_SSID "Iphone7"
+#define WIFI_PASSWORD "123456789"
 
 #define FIREBASE_URL "ionicarduino-2393a.firebaseio.com"
 #define FIREBASE_AUTH "lelyI1Z6qbaHrTaU39aYKXZCCUV9CPcT3jVLOVSA"
