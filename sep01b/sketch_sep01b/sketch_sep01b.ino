bae7f98cbfcdd97bd49d4b11cacb7d85b03a22da
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-09-01 14:42:44 +0700

    Fixed Wi-Fi problems

--- sep01b/sketch_sep01b/sketch_sep01b.ino
+++ sep01b/sketch_sep01b/sketch_sep01b.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-#define WIFI_SSID "Iphone7"
-#define WIFI_PASSWORD "123456789"
+#define WIFI_SSID "Jet"
+#define WIFI_PASSWORD "12345678"
 
 #define FIREBASE_URL "ionicarduino-2393a.firebaseio.com"
 #define FIREBASE_AUTH "lelyI1Z6qbaHrTaU39aYKXZCCUV9CPcT3jVLOVSA"
